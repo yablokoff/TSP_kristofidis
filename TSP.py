a138ed8e1b89bed30e0997b92fb5c022645cf9ee
Dmitrii Vlasov <yablokoff.tlt@gmail.com>
2012-12-09 23:18:48 +0400

    TSP withEuler .

--- TSP.py
+++ TSP.py
@@ -1,8 +1,9 @@
-class MST:
+from collections import defaultdict
+
+class TSP:
 
     dists = []
     vertex_queue = {}
-    MST_nodes=[]
 
     def __init__(self, cities_count):
         self.CITIES_COUNT = cities_count
@@ -21,9 +22,11 @@ class MST:
         del self.vertex_queue[current_city[0]]
         return current_city
 
-    def find(self):
+    def find_MST(self):
         new_city = self._extract_min()
-        self.MST_nodes.append(new_city)
+        MST_graph = list()
+        MST_nodes = list()
+        MST_nodes.append(new_city)
         while self.vertex_queue:
             new_city_id = new_city[0]
             for city in self.vertex_queue.items():
@@ -32,8 +35,87 @@ class MST:
                     self.vertex_queue[city_id]["distance"] = self.dists[new_city_id][city_id]
                     self.vertex_queue[city_id]["parent"] = new_city_id
             new_city = self._extract_min()
-            self.MST_nodes.append(new_city)
+            MST_nodes.append(new_city)
+        for node in MST_nodes[1:]:
+            MST_graph.append( (node[1]["parent"], node[0]) )
+        return MST_graph
+
+    def find_odd_nodes(self, MST_graph):
+        odd_nodes = list()
+        adjacent_edges_count = defaultdict(int)
+        for from_node, to_node in MST_graph:
+            adjacent_edges_count[from_node] += 1
+            adjacent_edges_count[to_node] += 1
+        for node_id, count in adjacent_edges_count.iteritems():
+            if count % 2:
+                odd_nodes.append(node_id)
+        return odd_nodes
+
+    def _calc_cost_of_edges(self, edges):
+        sum_cost = 0
+        for edge in edges:
+            sum_cost += self.dists[edge[0]][edge[1]]
+        return sum_cost
+
+    def _min_perfect_matching(self, chosen_edges, nodes):
+        if len(nodes) == 2:
+            chosen_edges.append((nodes[0], nodes[1]))
+            cost = self._calc_cost_of_edges(chosen_edges)
+            if cost < self.min_cost:
+                self.min_cost = cost
+                self.edges = chosen_edges
+            return
+        for first_node in nodes:
+            nodes_wo_first = nodes[:]
+            nodes_wo_first.remove(first_node)
+            for sec_node in nodes_wo_first:
+                nodes_wo_both = nodes_wo_first[:]
+                nodes_wo_both.remove(sec_node)
+                chosen_edges.append( (first_node, sec_node) )
+                self._min_perfect_matching(chosen_edges[:], nodes_wo_both)
+                chosen_edges.pop()
+
+    def find_minimum_perfect_matching(self, nodes):
+        self.min_cost = 10**6
+        self.edges = list()
+        self._min_perfect_matching([], nodes)
+        return self.edges
+
+    def extend_MST_with_perfect_matchings(self, MST):
+        odd_nodes = self.find_odd_nodes(MST)
+        perfect_matching_edges = self.find_minimum_perfect_matching(odd_nodes)
+        MST.extend(perfect_matching_edges)
+        return MST
+
+    def _get_first_neighbor(self, node, edges):
+        for first_node, sec_node in edges:
+            if first_node == node:
+                return sec_node
+            elif sec_node == node:
+                return first_node
+        return None
+
+    def find_euler_circuit(self, edges):
+        path = [0]
+        while len(edges):
+            for node in path:
+                if self._get_first_neighbor(node, edges):
+                    break
+            while self._get_first_neighbor(node, edges):
+                neighbor = self._get_first_neighbor(node, edges)
+                try:
+                    edges.remove( (node, neighbor) )
+                except ValueError:
+                    edges.remove( (neighbor, node) )
+                if not len(edges):
+                    path.append(node)
+                else:
+                    path.append(neighbor)
+                node = neighbor
+        return path
 
 if __name__ == "__main__":
-    inst = MST(15)
-    print inst.find()
+    inst = TSP(15)
+    MST_graph = inst.find_MST()
+    extended_MST = inst.extend_MST_with_perfect_matchings(MST_graph)
+    euler_path = inst.find_euler_circuit(extended_MST)
